efd7880cc7dcaba9e5f80eb71dd1fee848a7540b
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-25 17:03:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,8 +19,7 @@ def assign_rooms(name_array)
 end   
 
 def printer(name_array)
-  batch_badge_creator()
-  # assign_rooms(name_array).each do |name|
-  #   puts name 
-  # end   
+  assign_rooms(name_array).each do |name|
+    puts name 
+  end   
 end   
